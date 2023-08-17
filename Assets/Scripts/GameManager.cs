f0980bb35071b014df0d4d246aa261d6f600f2a9
CAHC2509 <111461283+CAHC2509@users.noreply.github.com>
2023-08-16 20:04:32 -0500

    Enhancements to Gameplay Mechanics
    - The main button's behavior has been diversified to dynamically adapt based on the current match state.
    - Players can now conveniently initiate or halt the match by using the space key.
    - After placing a bet, players are guided to the match screen, where they can commence the match through either the space key or the main button.
    
    These changes aim to provide a more interactive and streamlined gaming experience.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityEngine;
 using TMPro;
@@ -12,13 +13,29 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI multiplierText;
     [SerializeField]
-    private TextMeshProUGUI resultText;
-    [SerializeField]
     private GameObject popUpObject;
+    [SerializeField]
+    private TextMeshProUGUI resultText;
+
+    [Space, Header("Main button settings")]
+    [SerializeField]
+    private Button mainButton;
+    [SerializeField]
+    private Image mainButtonImage;
+    [SerializeField]
+    private TextMeshProUGUI mainButtonText;
+    [SerializeField]
+    private Color redColor;
+    [SerializeField]
+    private Color greenColor;
+    [SerializeField]
+    private UnityEvent buttonFeedbackEvent;
 
     [Space, Header("Gameplay settings")]
     [SerializeField]
     private float bombExplodeChance = 0.5f; // 50% chance of exploding per second
+    [SerializeField]
+    private float initialDelay = 0.25f; // Initial delay in seconds
 
     private float currentMultiplier = 1.0f;
     private float multiplierIncreaseRate = 0.2f;
@@ -34,15 +51,11 @@ public class GameManager : MonoBehaviour
             multiplierText.text = "Multiplier: x" + currentMultiplier.ToString("F2");
 
             if (Random.value < bombExplodeChance * Time.deltaTime)
-            {
                 ExplodeBomb();
-            }
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            EndRound();
-        }
+            mainButton.onClick.Invoke();
     }
 
     private void ExplodeBomb()
@@ -50,26 +63,31 @@ public class GameManager : MonoBehaviour
         bombExploded = true;
         canEndRound = false;
 
+        mainButton.onClick.RemoveAllListeners();
+
         resultText.text = $"BOMB EXPLODED!\nYou lost {currentBet} coins";
         popUpObject.SetActive(true);
 
         currentMultiplier = 1.0f; // Reset multiplier
     }
 
-    public void PlaceBet()
+    private void PlaceBet()
     {
         if (int.TryParse(betInputField.text, out currentBet))
         {
             canEndRound = true;
             bombExploded = false;
-            popUpObject.SetActive(false);
+
+            SetMainButtonToStop();
         }
     }
 
-    public void EndRound()
+    private void EndRound()
     {
         if (canEndRound)
         {
+            mainButton.onClick.RemoveAllListeners();
+
             int winnings = Mathf.FloorToInt(currentBet * currentMultiplier);
             resultText.text = "Congratulations!\nYou won " + winnings + " coins!";
             popUpObject.SetActive(true);
@@ -79,5 +97,33 @@ public class GameManager : MonoBehaviour
         {
             resultText.text = "Too late!\nYou lost " + currentBet + " coins.";
         }
+    }
+
+    public void SetMainButtonToStart()
+    {
+        mainButton.onClick.RemoveAllListeners();
+        mainButton.onClick.AddListener(() => StartCoroutine(StartMatchWithDelay()));
+        mainButton.onClick.AddListener(buttonFeedbackEvent.Invoke);
+
+        mainButtonImage.color = greenColor;
+        mainButtonText.text = "Start";
+
+        multiplierText.text = "Multiplier: x1.00";
+    }
+
+    private void SetMainButtonToStop()
+    {
+        mainButton.onClick.RemoveAllListeners();
+        mainButton.onClick.AddListener(EndRound);
+        mainButton.onClick.AddListener(buttonFeedbackEvent.Invoke);
+
+        mainButtonImage.color = redColor;
+        mainButtonText.text = "Stop!";
+    }
+
+    private IEnumerator StartMatchWithDelay()
+    {
+        yield return new WaitForSeconds(initialDelay);
+        PlaceBet();
     }
 }
